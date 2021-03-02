83dd0f58b222c7025939043a2661357994d76d76
hiroya <hiroya19890721@gmail.com>
2021-03-02 19:37:11 +0900

    javascript_pack_tagに変更、webpackerがコンパイル*

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,12 +1,3 @@
-const {
-  environment
-} = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker')
 
-
-const webpack = require('@rails/webpacker')
-environment.plugins.append('Provide', new webpack.ProvidePlugin({
-  $: 'jquery',
-  jquery: 'jquery',
-  popper: ['popper.js', 'default']
-}))
 module.exports = environment
