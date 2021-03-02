3c395aa886b9cfc1d2a63d5c14e9e5f04e58692e
hiroya <hiroya19890721@gmail.com>
2021-03-02 20:07:00 +0900

    日本語、タイムゾーンも変更

--- config/application.rb
+++ config/application.rb
@@ -23,6 +23,10 @@ module WeightManagement
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
+     # 日本語に変更
+     config.i18n.default_locale = :ja
+     # タイムゾーンも変更
+     config.time_zone = 'Asia/Tokyo'
     
 
     # Settings in config/environments/* take precedence over those specified here.
