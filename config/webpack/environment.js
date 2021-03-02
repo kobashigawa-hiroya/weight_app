d49d339813a89a5b49fe382d4ed9a01f404c0247
hiroya <hiroya19890721@gmail.com>
2021-03-02 12:50:30 +0900

    enironment.js変更

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,12 @@
-const { environment } = require('@rails/webpacker')
+const {
+  environment
+} = require('@rails/webpacker')
 
+
+const webpack = require('@rails/webpacker')
+environment.plugins.append('Provide', new webpack.ProvidePlugin({
+  $: 'jquery',
+  jquery: 'jquery',
+  popper: ['popper.js', 'default']
+}))
 module.exports = environment
