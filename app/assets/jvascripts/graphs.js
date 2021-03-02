78a4ff004220a3d24949cac903444e3eeb99704b
hiroya <hiroya19890721@gmail.com>
2021-03-02 21:37:50 +0900

    drawGraphToToday作成

--- app/assets/jvascripts/graphs.js
+++ app/assets/jvascripts/graphs.js
@@ -1,50 +1,90 @@
 document.addEventListener('turbolinks:load', () => {
-  // ここに， 「app/views/graphs/index.html.erb」の「script」タグ内をここに移動
-  let lineLabel = gon.chart_label
-  let lineData = gon.chart_data
-  // 折れ線グラフのオプション
 
-  let lineLabel = ['1/1', '1/2', '1/4', '1/5', '1/6', '1/7']
-  let lineData = [60.3, 61.1, 60.8, null, 60.5, 61.4]
+  // 日付の古い方・新しい方を取得する関数
+  const minDate = (date1, date2) => (date1 < date2) ? date1 : date2
+  const maxDate = (date1, date2) => (date1 > date2) ? date1 : date2
 
-  // 折れ線グラフのオプション
+  // データの初日・最終日
+  const START_DATE = convertDate(gon.weight_records[0].date)
+  const END_DATE = convertDate(gon.weight_records[gon.weight_records.length - 1].date)
+  // setHoursを使用しないと，時差の影響で0時にならないため注意！
+  const convertDate = (date) => new Date(new Date(date).setHours(0, 0, 0, 0))
 
-  const lineChartData = {
-    labels: lineLabel,
-    datasets: [{
-      label: '体重(kg)',
-      data: lineData,
-      backgroundColor: 'rgba(255, 99, 132, 0.2)',
-      borderColor: 'rgba(255, 99, 132, 1)',
-      borderWidth: 1,
-      spanGaps: true
-    }]
-  }
+  const TODAY = convertDate(new Date())
+  const A_WEEK_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 6)
+  const TWO_WEEKS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth(), TODAY.getDate() - 13)
+  const A_MONTH_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 1, TODAY.getDate() + 1)
+  const THREE_MONTHS_AGO = new Date(TODAY.getFullYear(), TODAY.getMonth() - 3, TODAY.getDate() + 1)
 
-  const lineChartOption = {
-    title: {
-      display: true,
-      text: '折れ線グラフ'
-    },
-    tooltips: {
-      callbacks: {
-        // ホバー（スマホならタップ）時のラベル表示を変更
-        title: function (tooltipItems) {
-          return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
-        },
-        label: function (tooltipItem) {
-          return '体重: ' + tooltipItem.yLabel + 'kg'
+  // グラフを描く場所を取得
+  const chartWeightContext = document.getElementById("chart-weight").getContext('2d')
+  let chartWeight
+  // 期間を指定してグラフを描く
+  const drawGraph = (from, to) => {
+    // from から to までの期間のデータに絞る
+    let records = gon.weight_records.filter((record) => {
+      let date = convertDate(record.date)
+      return from <= date && date <= to
+    })
+
+    // 日付のみのデータを作成
+    let dates = records.map((record) => {
+      // 横軸のラベル表示は簡潔にしたいので，
+      // 日付 2020-01-08 を 1/8 のような形式に変換する
+      return record.date.replace(/^\d+-0*(\d+)-0*(\d+)$/, '$1/$2')
+    })
+
+    // 体重のみのデータを作成
+    let weights = records.map((record) => record.weight)
+
+    let weightData = {
+      labels: dates,
+      datasets: [{
+        label: '体重(kg)',
+        data: weights,
+        backgroundColor: 'rgba(255, 99, 132, 0.2)',
+        borderColor: 'rgba(255, 99, 132, 1)',
+        borderWidth: 1,
+        spanGaps: true
+      }]
+    }
+
+    let weightOption = {
+      tooltips: {
+        callbacks: {
+          // ホバー（スマホならタップ）時のラベル表示を変更
+          title: function (tooltipItems) {
+            return tooltipItems[0].xLabel.replace(/^(\d+).(\d+)$/, ' $1 月 $2 日')
+          },
+          label: function (tooltipItem) {
+            return '体重: ' + tooltipItem.yLabel + 'kg'
+          }
         }
       }
     }
+
+    if (!chartWeight) {
+      // グラフが存在しないときは，作成する
+      chartWeight = new Chart(chartWeightContext, {
+        type: 'line',
+        data: weightData,
+        options: weightOption
+      })
+    } else {
+      // グラフが存在するときは，更新する
+      chartWeight.data = weightData
+      chartWeight.options = weightOption
+      chartWeight.update()
+    }
   }
 
-  // 折れ線グラフを表示
-  const lineChartContext = document.getElementById("line-chart").getContext('2d')
-  new Chart(lineChartContext, {
-    type: 'line',
-    data: lineChartData,
-    options: lineChartOption
-  })
+  // グラフの初期表示
+  drawGraph(A_WEEK_AGO, TODAY) // 引数の日付から今日までのグラフを描く関数
+  const drawGraphToToday = (from) => {
+    // データが存在する範囲に修正
+    from = maxDate(from, START_DATE)
+    let to = minDate(TODAY, END_DATE)
+    drawGraph(from, to)
+  }
 
 })
